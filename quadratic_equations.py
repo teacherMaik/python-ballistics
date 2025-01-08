5b81a0ec0d95a1549a860231f9de18416367fae3
Mikelele <d.michaelthomasbennett@gmail.com>
2025-01-08 22:05:39 +0100

    first file working

---
+++ quadratic_equations.py
@@ -0,0 +1,19 @@
+import cmath;
+import math;
+
+a = float(input("define value for 'a'"));
+b = float(input("define value for 'b'"));
+c = float(input("define value for 'c'"));
+
+def solve_quadratic(a, b, c):
+
+    discriminant = b**2 - 4*a*c;
+
+    root_1 = (-b + cmath.sqrt(discriminant)) / (2 * a)
+    root_2 = (-b - cmath.sqrt(discriminant)) / (2 * a)
+    print("root 1", root_1, "root 2", root_2);
+
+solve_quadratic(a, b, c);
+
+
+
