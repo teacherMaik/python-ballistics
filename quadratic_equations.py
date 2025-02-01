f3fe5ae20123cabdd76fc61bdcaf987f832d2d16
Mikelele <d.michaelthomasbennett@gmail.com>
2025-02-01 22:44:15 +0100

    One commit a day

--- quadratic_equations.py
+++ quadratic_equations.py
@@ -7,13 +7,13 @@ c = float(input("define value for 'c'"));
 
 def solve_quadratic(a, b, c):
 
-    discriminant = b**2 - 4*a*c;
+    discriminant = b**2 - 4*a*c
 
     root_1 = (-b + cmath.sqrt(discriminant)) / (2 * a)
     root_2 = (-b - cmath.sqrt(discriminant)) / (2 * a)
-    print("root 1", root_1, "root 2", root_2);
+    print("root 1", root_1, "root 2", root_2)
 
-solve_quadratic(a, b, c);
+solve_quadratic(a, b, c)
 
 
 
